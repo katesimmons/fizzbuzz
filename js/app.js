c0018022b15f580735841d189ddc59eb5dca0239
Kate Simmons <kate.a.simmons@gmail.com>
2015-08-04 22:00:19 -0500

    formatting

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 var i;
 for (i = 1; i <= 15; i++) {
-if ((i%3 == 0) && (i%5 == 0)) {
-document.write('FizzBuzz<br>');
-}
-else if (i%3 == 0) {
-document.write('Fizz<br>');
-}
-else if (i%5 == 0) {
-document.write('Buzz<br>');
-}
-else {
-document.write(i + '<br>');
-}
+    if ((i%3 == 0) && (i%5 == 0)) {
+    document.write('FizzBuzz<br>');
+    }
+    else if (i%3 == 0) {
+    document.write('Fizz<br>');
+    }
+    else if (i%5 == 0) {
+    document.write('Buzz<br>');
+    }
+    else {
+    document.write(i + '<br>');
+    }
 }
